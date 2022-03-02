4a4eb48b5891978ae974773e4fe9b56dc7c3597f
Catarina <catarinasaomiguel@tecnico.ulisboa.pt>
2022-03-02 02:05:40 +0000

    Delete unused code

--- functions/index.js
+++ functions/index.js
@@ -4,16 +4,9 @@ const https = require('https');
 const cors = require('cors')
 const config = require('./config.js');
 const axios = require('axios');
-//const { resolve } = require("path");
 
-//const { json } = require('express/lib/response');
-//const res = require('express/lib/response');
 const app = express();
 
-
-//let token = ''; 
-//const stationCodes = new Map();
-//const destinationCodes = new Map();
 app.use(cors());
 app.options('*', cors());
 
@@ -21,37 +14,17 @@ app.use(express.urlencoded({
     extended: true
 }))
 
-
-// Starts app
-app.get('/', (request, response) => {
-    /*var tokenOptions = {
-        host: 'api.metrolisboa.pt',
-        port: '8243',
-        path: '/token',
-        method: 'POST',
-        headers : { 'Authorization' : `Basic ${config.BASE64_SECRET}`,
-        'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    };
-    let token = '';
-    doRequest(tokenOptions).then((data) => {token = data.access_token; console.log(token)});*/
-    //TODO: handle async and error
-    //storeStationCodes();
-    //storeDestinationCodes();
-});
-
-//requestToken(); TODO: maybe better
 //app.listen(process.env.PORT || 3000, () => console.log("Server available on http://localhost:3000"))
 
-// Request for 
+/**
+ * Endpoint for requesting waiting times
+ */
 app.get('/metro', (request, response) => {
-    // TODO: use let stationCode = findStationCode("Oriente"); instead of storing all
     requestToken().then((token) => {
         console.log(token);
         storeStationCodes(token).then((stationCodes) => {
             console.log("FINISHED STATIONS!" + stationCodes);
             storeDestinationCodes(token).then((destinationCodes) => {
-                //console.log("FINISHED ALL!" + stationCodes.get(request.query.station) + " " + destinationCodes.get(request.query.destination));
                 var optionsMetro = {
                     url: `https://api.metrolisboa.pt:8243/estadoServicoML/1.0.1/tempoEspera/Estacao/${stationCodes.get(request.query.station)}`,
                     host: 'api.metrolisboa.pt',
@@ -69,21 +42,16 @@ app.get('/metro', (request, response) => {
                             if(data.resposta[el].hasOwnProperty('destino')) {
                                 if(data.resposta[el].destino == destinationCodes.get(request.query.destination)) {
                                     result = data.resposta[el];
-                                    console.log(data.resposta[el]);
                                     response.send(data.resposta[el]); 
                                     return;
                                 }
                             }
                         }
             
-                    }       
-                    console.log("RESULT: " + result.resposta);
+                    }
                     response.send(result); 
             });
         });
-    
-    //let stationCodes = storeStationCodes();
-    //let destinationCodes = storeDestinationCodes();
     });
 });
 });
@@ -91,7 +59,7 @@ app.get('/metro', (request, response) => {
 
 
 /**
- * Do a get request with options provided.
+ * Do a get request with options provided
  *
  * @param {Object} options
  * @return {Promise} a promise of request
@@ -107,7 +75,6 @@ function doRequest(options) {
         });
 
         res.on('end', () => {
-            //console.log(responseBody);
             resolve(JSON.parse(responseBody));
         });
         });
@@ -119,21 +86,21 @@ function doRequest(options) {
         if(options.method == 'POST')
             req.write('grant_type=client_credentials');
         req.end();
-    
-    /*/console.log(options);
-    axios(options).then(function (response) {
-        console.log(response);
-        resolve(response);
-*/
     });
-//});
 }
 
+/**
+ * TODO: Handles errors
+ */
 function processError(err) {
     console.error("ERROR: " + err);
 }
 
-
+/**
+ * Requests API access token
+ * @return {Promise} a promise of request
+ * @return {String} token
+ */
 function requestToken() {
     return new Promise((resolve, reject) => {
         var tokenOptions = {
@@ -158,6 +125,9 @@ function requestToken() {
 
 /**
  * Returns stations codes
+ * @param {String} token for API request
+ * @return {Promise} a promise of request
+ * @return {Map} maps key station name to its code
  */
 function storeStationCodes(token) {
     return new Promise((resolve, reject) => {
@@ -176,15 +146,16 @@ function storeStationCodes(token) {
                 stationCodes.set(data.resposta[el].stop_name, data.resposta[el].stop_id);
             }
             resolve(stationCodes);
-
         });
-        //resolve(stationCodes);
     });
 }
 
 
 /**
  * Returns destination codes
+ * @param {String} token for API request
+ * @return {Promise} a promise of request
+ * @return {Map} maps key destination name to its code
  */
 function storeDestinationCodes(token) {
     return new Promise((resolve, reject) => {
@@ -204,7 +175,6 @@ function storeDestinationCodes(token) {
             }
             resolve(destinationCodes);
         });
-        //resolve(destinationCodes);
     });
 }
 
